b5f617dc7271a28762860c6ab80ee3ae21f2e21d
imaculate-echesa <immaculatezepwa18@gmail.com>
2022-08-24 17:50:25 +0300

    Done with  assignment

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
-// Your code here
+ //Your code here
+ const leftNumbers = dodger.style.left.replace("px", "");
+ const left = parseInt(leftNumbers, 10);
+ const dodger = document.getElementById("dodger");
+ function moveDodgerLeft() {
+   if (left > 0) {
+       dodger.style.left = `${left - 1}px`;
+     }
+   }
+
+  function moveDodgerRight() {
+    if (left > 0) {
+      dodger.style.left = `${left + 1}px`;
+     }
+  }
+   
+   
+   
+   
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// concati("Philip", "going")
+// concati("Lucy", "walking")
+
+// noun    verb
+// Mary is going to school
+
+
+  
